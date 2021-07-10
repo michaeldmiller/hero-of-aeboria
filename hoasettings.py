e79ac6de4041b33dd62b315ee4e3b09a7e733e4a
Michael D. Miller <michaeldmiller3125@gmail.com>
2021-07-10 17:26:38 -0400

    v0.2.1 distributes code into multiple specific files

---
+++ hoasettings.py
@@ -0,0 +1,12 @@
+# hero of aeboria settings file
+# all relevant setting variables, including window size, terrain blocks, gravity, and frame-rate
+
+target_frame_rate = 60
+window_x_size = 1000
+window_y_size = 500
+terrain_list = [(500, 400, 5000, 20), (300, 350, 100, 50), (500, 325, 100, 50), (700, 300, 100, 50),
+                (900, 300, 200, 50), (1100, 300, 50, 500), (-200, 310, 350, 50), (-210, 315, 100, 75),
+                (-260, 350, 30, 50)]
+gravity = 0.35
+friction_constant = -0.07
+side_scroll_speed = 1
